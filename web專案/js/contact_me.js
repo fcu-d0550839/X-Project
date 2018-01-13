5b17862e64a92b2dc11bdbd94a3115e7127fe712
www-PC\www <evil81654@gmail.com>
2018-01-13 15:31:05 +0800

    修正信箱接收 增加money訊息 修改聯絡我們文本

--- web專案/js/contact_me.js
+++ web專案/js/contact_me.js
@@ -27,10 +27,11 @@ $(function() {
           name: name,
           phone: phone,
           email: email,
-          message: message
+          message: message,
+		  money: money
         },
         cache: false,
-        error: function() {
+        success: function() {
           // Success message
           $('#success').html("<div class='alert alert-success'>");
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
@@ -40,7 +41,7 @@ $(function() {
 		  $('#success > .alert-success')
             .append('</br>');
           $('#success > .alert-success')
-            .append($("<strong>").text("以下是你的留言 將會由我們轉答 ："));
+            .append($("<strong>").text("以下是你的留言 將會由我們轉答："));
 		  $('#success > .alert-success')
             .append('</br>');	
 		  $('#success > .alert-success')
@@ -52,12 +53,12 @@ $(function() {
 		  $('#paycard').hide();
 		  $('#postpay').hide();
         },
-        success: function() {
+        error: function() {
           // Fail message
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append($("<strong>").text("很抱歉 " + firstName + ", 現在伺服器可能沒有回應 無法接收你的訊息"));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
